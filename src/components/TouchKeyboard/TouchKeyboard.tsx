80cc0bc5ee2c083eea5f7cdc5b435ea4098e96ec
Mike Griffith <bikegriffith@gmail.com>
2026-02-13 02:08:43 -0500

    spacing

--- src/components/TouchKeyboard/TouchKeyboard.tsx
+++ src/components/TouchKeyboard/TouchKeyboard.tsx
@@ -11,8 +11,8 @@ import {
 type AlphaMode = 'alpha' | 'special' | 'emoji';
 
 const ROW1 = 'qwertyuiop';
-const ROW2 = 'asdfghjkl';
-const ROW3 = 'zxcvbnm';
+const ROW2 = "asdfghjkl;'";
+const ROW3 = 'zxcvbnm,./';
 
 const SPECIAL_ROW1 = '@#$%^&*()';
 const SPECIAL_ROW2 = '!"\'+,-./:;';
@@ -67,14 +67,17 @@ function BackspaceIcon({ className }: { className?: string }) {
   return (
     <svg
       className={className}
-      fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+      stroke-linejoin="round"
+      stroke-miterlimit="2"
     >
-      <path d="M21 12H6M6 12L10 8M6 12L10 16" />
+      <path
+        d="m22 6c0-.552-.448-1-1-1h-12.628c-.437 0-.853.191-1.138.523-1.078 1.256-3.811 4.439-4.993 5.815-.16.187-.241.419-.241.651 0 .231.08.463.24.651 1.181 1.38 3.915 4.575 4.994 5.835.285.333.701.525 1.14.525h12.626c.552 0 1-.448 1-1 0-2.577 0-9.423 0-12zm-13.628.5h12.128v11h-12.126l-4.715-5.51zm5.637 4.427 1.71-1.71c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.384-.219.531l-1.711 1.711 1.728 1.728c.147.147.22.339.22.53 0 .427-.349.751-.75.751-.192 0-.384-.073-.531-.219l-1.728-1.729-1.728 1.729c-.146.146-.339.219-.531.219-.401 0-.75-.324-.75-.751 0-.191.073-.383.22-.53l1.728-1.728-1.788-1.787c-.146-.148-.219-.339-.219-.532 0-.425.346-.749.751-.749.192 0 .384.073.53.219z"
+        fill-rule="nonzero"
+      />
     </svg>
   );
 }
@@ -150,7 +153,7 @@ export default function TouchKeyboard() {
         >
           <div className="h-full flex justify-center items-end px-4 pb-3 pt-1">
             {/* Centered keyboard content */}
-            <div className="flex gap-4 items-end max-w-2xl">
+            <div className="flex gap-12 items-end justify-center max-w-2xl">
               {/* Alpha section */}
               <div className="flex flex-col justify-end gap-2 shrink-0">
                 {/* Row 1: letters/special/emoji + delete */}
@@ -272,7 +275,7 @@ export default function TouchKeyboard() {
                     ))}
                 </div>
                 {/* Row 4: Special | Emoji | Voice | Space | Done */}
-                <div className="flex gap-2 items-stretch">
+                <div className="flex gap-2 items-stretch pl-12">
                   <motion.button
                     type="button"
                     tabIndex={-1}
@@ -291,7 +294,7 @@ export default function TouchKeyboard() {
                     transition={{ type: 'spring', stiffness: 400, damping: 25 }}
                     aria-label="Special characters"
                   >
-                    <Hash className="w-5 h-5" />
+                    {'!@'}
                   </motion.button>
                   <motion.button
                     type="button"
